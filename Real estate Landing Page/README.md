4f7ce87c3d80ea2d0fea9e65e42a8f35084c28c4
Sunday Ugochukwu Emmanuel <ugochukwu2705@gmail.com>
2022-12-01 13:09:02 +0100

    Delete Real estate Landing Page directory

--- Real estate Landing Page/README.md
+++
@@ -1,3 +0,0 @@
-# Real estate landing page sample.
-
-## Images gotten from www.freepik.com  www.shutterstock.com
--- Real estate Landing Page/server.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const fs = rquire('fs')
-const http = require('http')
-
-const server = http.createServer((req, res) =>{
-  let filePath = path.join(__dirname, 'app', req.url === '/' ? 'Home.html': req.url)
-  let contentType = getContentType(filePath) || 'text/html'
-})
